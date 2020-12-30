cd7864161b844a4b4afe3105ef8525a810cdd6e8
zuttti25 <xxx.pxjpp@icloud.com>
2020-12-30 06:05:47 +0000

    いいね&コメント機能の実装完了

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update,:create]
   resources :books do
+    resource :favorites, only: [:create, :destroy]
+    #単数にすると、そのコントローラのidがリクエストに含まれなくなる。
     resources :book_comments, only: [:create, :destroy]
   end
   root 'homes#top'
